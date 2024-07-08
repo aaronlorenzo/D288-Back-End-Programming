99f22488ad7f9f2e8a9803ef395172f7b397bdfe
Admin <agarrol@wgu.edu>
2024-07-07 18:08:00 -0700

    part G done

--- src/main/java/com/example/demo/entities/Customer.java
+++ src/main/java/com/example/demo/entities/Customer.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
@@ -22,24 +23,24 @@ public class Customer {
     @Column(name = "customer_id")
     private Long id;
 
-    @NonNull
-    @Column(name = "customer_first_name")
+
+    @Column(name = "customer_first_name", nullable = false)
     private String firstName;
 
 
-    @Column(name = "customer_last_name")
+    @Column(name = "customer_last_name", nullable = false)
     private String lastName;
 
 
-    @Column(name = "address")
+    @Column(name = "address", nullable = false)
     private String address;
 
 
-    @Column(name = "postal_code")
+    @Column(name = "postal_code", nullable = false)
     private String postal_code;
 
 
-    @Column(name = "phone")
+    @Column(name = "phone", nullable = false)
     private String phone;
 
     @CreationTimestamp
@@ -58,5 +59,12 @@ public class Customer {
     private Set<Cart> carts;
 
     public void add(Cart cart) {
+        if (cart !=null) {
+            if (carts ==null) {
+                carts = new HashSet<>();
+            }
+            carts.add(cart);
+            cart.setCustomer(this);
+        }
     }
 }
